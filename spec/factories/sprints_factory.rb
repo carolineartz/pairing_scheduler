08aa11eae8c79e8bb37edb09d30621463aa40df1
Caroline Artz <ceartz@gmail.com>
2020-03-07 15:35:47 -0600

    Cleanup sprints factory.

--- spec/factories/sprints_factory.rb
+++ spec/factories/sprints_factory.rb
@@ -4,19 +4,5 @@ FactoryBot.define do
 
     start_date { project.start_date }
     end_date { project.end_date }
-
-    after(:build) do |sprint, evaluator|
-      if evaluator.project
-        # why is this being weird and I have to do these associations manually?
-        evaluator.project.save! if evaluator.project.new_record?
-        sprint.project = evaluator.project
-        # sprint.project = evaluator.project
-        # evaluator.project.sprints << sprint
-        ap sprint
-        ap evaluator.project
-      end
-
-      # evaluator.project.sprints << sprint
-    end
   end
 end
