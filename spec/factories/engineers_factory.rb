22dfdb3737dfa548c31322a58f7b4b1f83d5d796
Caroline Artz <ceartz@gmail.com>
2020-03-06 19:28:21 -0600

    Fixup engineers and projects factories.

--- spec/factories/engineers_factory.rb
+++ spec/factories/engineers_factory.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :engineer do
     display_name { Faker::Name.first_name }
+
+    projects { |e| [e.association(:project)] }
   end
 end
--- spec/factories/projects_factory.rb
+++ spec/factories/projects_factory.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :project do
     sequence(:name) { |n| "Project-#{n}" }
+
+    start_date { Date.current.beginning_of_week }
+    end_date { Date.current.beginning_of_week + 4.days }
   end
 end
