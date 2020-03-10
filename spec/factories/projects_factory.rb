a1ea7a00eb5e9d2f1d510f7c246947db600a0afb
Caroline Artz <ceartz@gmail.com>
2020-03-09 19:21:33 -0500

    ProjectsFactory: Allow creating with_engineers trait given a list of members

--- spec/factories/projects_factory.rb
+++ spec/factories/projects_factory.rb
@@ -40,9 +40,10 @@ FactoryBot.define do
     trait :with_engineers do
       transient do
         engineer_count { 4 }
+        members { FactoryBot.create_list(:engineer, engineer_count) }
 
         after(:create) do |project, evaluator|
-          FactoryBot.create_list(:engineer, evaluator.engineer_count).tap do |eng|
+          evaluator.members.to_a.each do |eng|
             project.engineers << eng
           end
         end
