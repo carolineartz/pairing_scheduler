d61317e5b5fa10f86db429bcbdf6c79cb2cb9483
Caroline Artz <ceartz@gmail.com>
2020-03-10 13:54:27 -0500

    Add Grommet base.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Grommet, Box } from 'grommet'
+import { ThemeType } from 'grommet/themes/base'
+import { deepFreeze } from 'grommet/utils'
 
-export default App;
+const theme: ThemeType = deepFreeze({
+  global: {
+    colors: {
+      brand: '#81357D',
+      'accent-1': '#bd53b8',
+      'accent-2': '#500d4d',
+      focus: '#bd53b8',
+    },
+    font: {
+      family: 'Montserrat',
+      size: '16px',
+    },
+  },
+})
+
+const App = () => (
+  <Grommet theme={theme} full={true}>
+    <Box>App Placeholder</Box>
+  </Grommet>
+)
+
+export default App
